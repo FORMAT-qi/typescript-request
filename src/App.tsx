343619e3407068b64a4cb3573d14f5ba56368f35
jiashiqi <jiashiqi@duliday.com>
2020-11-12 17:49:52 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React, { useState }from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {getRiskFileAllTypeList} from './services/ToolService';
-import FileTypeModel from 'model/FileTypeModel';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getRiskFileAllTypeList } from "./services/ToolService";
+import FileTypeModel from "model/FileTypeModel";
 
 function App() {
-  const  [dataList, setDataListData] =  useState<FileTypeModel[]>([]);
+  const [dataList, setDataListData] = useState<FileTypeModel[]>([]);
   return (
     <div className="App">
-       <button onClick={ async ()=> {
-          setDataListData(await getRiskFileAllTypeList({"sort":"", "time":"", "key":""}));
-       }}>点击</button>
-       {dataList&&dataList.map(item => (<span key={item.id} style={{color:"red"}}>{item.typeName}</span> ))}
+      <button
+        onClick={async () => {
+          setDataListData(
+            await getRiskFileAllTypeList({ sort: "", time: "", key: "" })
+          );
+        }
+      }>点击</button>
+
+      {dataList.map((item) => (
+        <div key={item.id} style={{ color: "red" }}>
+          {item.typeName}
+          <div>
+            {item.materialTypeDTOList.map((child) => (
+              <span
+                key={child.id}
+              >{` ${child.typeName} `}</span>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
-  
 }
 
 export default App;
